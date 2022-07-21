e0c751440f1bd65c3da6703f64dcb203d296fcfc
Murad Nasibli <muradnasibli@Murads-MacBook-Pro.local>
2022-07-21 16:46:41 +0400

    Plus photo, text fields for email, pass,fullname, username added

--- InstagramFirestore/Controller/Authentication/RegistrationController.swift
+++ InstagramFirestore/Controller/Authentication/RegistrationController.swift
@@ -11,11 +11,67 @@ class RegistrationController: UIViewController {
     
     // MARK: - Properties
     
+    private let plushPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(imageLiteralResourceName: "plus_photo"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    private let emailTextField: CustomTextField = {
+        let tf = CustomTextField(placeholder: "Email")
+        tf.keyboardType = .emailAddress
+        
+        return tf
+    }()
+    
+    private let passwordTextField: CustomTextField = {
+        let tf = CustomTextField(placeholder: "Password")
+        tf.isSecureTextEntry = true
+        
+        return tf
+    }()
+    
+    private let fullNameTextField = CustomTextField(placeholder: "Fullname")
+    private let usernameTextField = CustomTextField(placeholder: "Username")
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemPurple
+        button.layer.cornerRadius = 5
+        button.setHeight(50)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI(){
+        configureGradientLayer()
         
-        view.backgroundColor = .systemPink
+        view.addSubview(plushPhotoButton)
+        plushPhotoButton.centerX(inView: view)
+        plushPhotoButton.setDimensions(height: 140, width: 140)
+        plushPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField,
+                                                       fullNameTextField, usernameTextField, signUpButton])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        view.addSubview(stackView)
+        stackView.anchor(top: plushPhotoButton.bottomAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
     }
 }
